7a6443bf08ec324d6abe5141edeb05458f80fb26
Niraj Kumar <n1r4jkumar@gmail.com>
2022-10-09 06:54:43 +0530

    pagination Demo added to the home page table

--- Dashboard design/js/sidebar.js
+++ Dashboard design/js/sidebar.js
@@ -32,7 +32,62 @@ function show(shown, hidden, active, active11) {
   }
 
   function loadHTML(){
-    fetch('home.html')
+    fetch('./html/home.html')
     .then(response=> response.text())
     .then(text=> document.getElementById('homePage').innerHTML = text);
   }
+
+
+
+  // pagination
+
+  const ulTag  = document.querySelector("#ulTag");
+  let totalPages = 20;
+
+  function element(totalPages, page){
+    let liTag = '';
+    let activeLi = '';
+    let beforePages = page -1;
+    let afterPages = page +1;
+    if(page > 1){
+        liTag += `<li class="btn prev" onclick="element(totalPages, ${page - 1})"><span>Prev</span></li>`;
+    }
+    if(page > 2){
+        liTag += `<li class="numb" onclick="element(totalPages, 1)"><span>1</span></li>`;
+        if(page > 3){
+            liTag += `<li class="dots"><span>...</span></li>`;
+        }
+    }
+    
+    for(let pageLength = beforePages; pageLength <= afterPages; pageLength++){
+        if(pageLength > totalPages){
+            continue;
+        }
+        if(pageLength == 0){
+            pageLength = pageLength +1;
+        }
+        if(page == pageLength){
+            activeLi = "pageActive";
+        }
+        else{
+            activeLi = "";
+        }
+        liTag += `<li class="numb ${activeLi}" onclick="element(totalPages, ${pageLength})"><span>${pageLength}</span></li>`;
+    }
+    if(page < totalPages - 1){
+        if(page < totalPages - 2){
+            liTag += `<li class="dots"><span>...</span></li>`;
+        }
+        liTag += `<li class="numb" onclick="element(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+    }
+
+    if(page < totalPages){
+        liTag += `<li class="btn next" onclick="element(totalPages, ${page + 1})"><span>Next</span></li>`;
+    }
+
+
+    ulTag.innerHTML = liTag; 
+
+  }
+
+  element(totalPages,2) //calling abouve function with passing values
